e08aec2d3bcbb6cbaafedade5d468398351544d2
James Swank <jmswank7@gmail.com>
2019-10-14 22:17:17 -0600

    stuff

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def get_layout():
     return html.Div(
         [
             html.Div([
-                html.H1(
+                html.H4(
                     'DENVER TEMPERATURE RECORD',
                     className='twelve columns',
                     style={'text-align': 'center'}
